85bf39a3a30703409f8b373fad151505d3aed989
Alan Timbó <alancorreiatimbo@gmail.com>
2023-06-05 01:41:12 -0300

    Initial settings

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+// export function init(): Promise<Express> {
+//     connectDb();
+//     return Promise.resolve(app);
+//   }
+  
+
+export default app;
